85919babf8b965f3188df726db97b7d95fc5a73b
Alex Stocking <alex.i.stocking@gmail.com>
2024-02-09 15:45:12 -0500

    feat: Reworked jump state slightly

--- Scenes/Player/Jump.gd
+++ Scenes/Player/Jump.gd
@@ -6,12 +6,12 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func enter(_msg := {}) -> void:
 	player = owner
-	player.velocity = Vector2(0, -400)
+	player.velocity.y = player.JUMP_VELOCITY
 	player.PlayerSprite.play("jump")
 	
 func physics_update(delta: float) -> void:
 	
-	if (!Input.is_action_pressed("Jump") || player.velocity.y > 0):
+	if (Input.is_action_just_released("Jump") || player.velocity.y > 0):
 		state_machine.transition_to("Falling")
 	
 	if(Input.is_action_just_pressed("Dash")):
@@ -21,5 +21,5 @@ func physics_update(delta: float) -> void:
 	player.handle_horizontal()
 	player.move_and_slide()
 	
-	if (player.is_on_floor() && !Input.is_action_pressed("Left") || !Input.is_action_pressed("Right")):
+	if (player.is_on_floor() && player.velocity.y == 0):
 		state_machine.transition_to("Idle")	
