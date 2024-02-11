580f6cd27ca2c9903def3d899a703831334f940f
David Stocking <dmstocking@gmail.com>
2024-02-10 19:07:44 -0500

    fix: Prevent rapidly switching betwween Idle and Run
    If you hold both left and right, you rapidly switch between idle and
    run. This just checks in idle that both left and right are not held.

--- Scenes/Player/States/Idle.gd
+++ Scenes/Player/States/Idle.gd
@@ -12,9 +12,12 @@ func update(delta: float) -> void:
 	if (!player.is_on_floor()):
 		state_machine.transition_to("Falling")
 	
+	var left = Input.is_action_pressed("Left")
+	var right = Input.is_action_pressed("Right")
+	
 	if Input.is_action_just_pressed("Jump"):
 		state_machine.transition_to("Jump")
-	elif Input.is_action_pressed("Left") or Input.is_action_pressed("Right"):
+	elif (left or right) and not (left and right):
 		state_machine.transition_to("Run")
 	elif Input.is_action_pressed("Dash"):
 		var direction = 1 if player.PlayerSprite.flip_h == true else -1
