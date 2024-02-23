71842672b404c00c1f799cf0ce193589ce314288
Alex Stocking <alex.i.stocking@gmail.com>
2024-02-22 21:50:42 -0500

    feat: Added Global scene

---
+++ Scenes/Misc/global.gd
@@ -0,0 +1,20 @@
+extends Node
+
+@onready var MusicPlayer = get_node("/root/BgmPlayer")
+
+var DefeatedBosses =  [false, false, false, false, false, false, false, false]
+var AcquiredArmors = [false, false, false, false]
+
+func _ready():
+	pass
+
+func changeMusic(path : String):
+	MusicPlayer.stop()
+	MusicPlayer.set_stream(path)
+	MusicPlayer.play()
+
+func saveGame():
+	pass
+
+func loadGame():
+	pass
