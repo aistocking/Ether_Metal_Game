c1c4cb910bf352958585b519dded85fee2e01bd0
Alex Stocking <alex.i.stocking@gmail.com>
2024-03-31 11:17:52 -0400

    refactor: moved the check for special attack input out of update

--- Scenes/Player/States/Idle.gd
+++ Scenes/Player/States/Idle.gd
@@ -11,24 +11,28 @@ func enter(_msg := {}) -> void:
 	player.CoyoteTimer.stop()
 
 func handle_input(event) -> void:
+	if Input.is_action_pressed("Offensive Trigger"):
+		if event.is_action_pressed("Face Buttons") && player.ChargeLevel != 0:
+			state_machine.transition_to("Special Attack", { "IsOffensive": true })
+	
+	if Input.is_action_pressed("Defensive Trigger"):
+		if event.is_action_pressed("Face Buttons") && player.ChargeLevel != 0:
+			state_machine.transition_to("Special Attack", { "IsOffensive": false })
+	
 	if event.is_action_pressed("Jump"):
 		state_machine.transition_to("Jump")
 	
 	if event.is_action_pressed("Dash"):
 		var direction = player.FacingDirection
 		state_machine.transition_to("Dash", { "direction": direction })
+	
+
 
 func physics_update(delta: float) -> void:
 	if !player.is_on_floor():
 		state_machine.transition_to("Falling")
 	
-	if Input.is_action_pressed("Offensive Trigger"):
-		if Input.is_action_just_pressed("Face Buttons") && player.ChargeLevel != 0:
-			state_machine.transition_to("Special Attack", { "IsOffensive": true })
-	
-	if Input.is_action_pressed("Defensive Trigger"):
-		if Input.is_action_pressed("Face Buttons") && player.ChargeLevel != 0:
-			state_machine.transition_to("Special Attack", { "IsOffensive": false })
+
 	
 	var left = Input.is_action_pressed("Left")
 	var right = Input.is_action_pressed("Right")
