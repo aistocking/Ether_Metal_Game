f5b71e3719faaca3c2991b29dd53e347ab6d1c8b
Alex Stocking <alex.i.stocking@gmail.com>
2024-03-31 14:53:29 -0400

    feat: Added bounces to disengage bombs

--- Scenes/Effects/small_bombs.gd
+++ Scenes/Effects/small_bombs.gd
@@ -6,13 +6,17 @@ var damage: int = 5
 
 var direction: int
 
+var bounces: int = 0
+
 func _ready():
 	velocity.x = direction * 100
 
 
 func _physics_process(delta):
+	if is_on_floor():
+		bounce()
 	velocity.y += gravity * delta
-	velocity = velocity.move_toward(Vector2.ZERO, 100 * delta)
+	velocity = velocity.move_toward(Vector2.ZERO, 100 * delta)	
 	move_and_slide()
 
 func explode():
@@ -20,6 +24,22 @@ func explode():
 
 func getDirection(dir):
 	direction = dir
+	if dir == 1:
+		$AnimatedSprite2D.flip_h = true
+	
+func bounce():
+	match bounces:
+		0:
+			velocity.y = -150
+			bounces += 1
+		1:
+			velocity.y = -80
+			bounces += 1
+		2:
+			velocity.y = -25
+			bounces += 1
+		3:
+			bounces += 1
 
 func _on_area_2d_area_entered(area):
 	if(area.has_method("takeDamage")):
