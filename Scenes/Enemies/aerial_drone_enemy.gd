25ea38d45f26b63a4c529815594cd32661009bdb
Alex Stocking <alex.i.stocking@gmail.com>
2024-04-02 12:48:30 -0400

    feat: Tracker enemy can now deal and take damage

--- Scenes/Enemies/aerial_drone_enemy.gd
+++ Scenes/Enemies/aerial_drone_enemy.gd
@@ -1,6 +1,12 @@
 extends CharacterBody2D
 
+var Health:int = 5
+var Damage:int = 1
+const ExplosionEffect = preload("res://Scenes/Effects/medium_explosion.tscn")
+
 var Player
+
+@onready var Sprite = $AnimatedSprite2D
 
 func _ready():
 	Player = get_tree().get_first_node_in_group("Player")
@@ -8,3 +14,25 @@ func _ready():
 func _physics_process(delta):
 	position = position.move_toward(Player.position, 50 * delta)
 	move_and_slide()
+
+func takeDamage(damage):
+	Health -= damage
+	if Health <= 0:
+		die()
+	hitFlash()
+
+func hitFlash():
+	Sprite.material.set_shader_parameter("active", true)
+	await get_tree().create_timer(0.1).timeout
+	Sprite.material.set_shader_parameter("active", false)
+
+func die():
+	var ExplosionInstance = ExplosionEffect.instantiate()
+	get_parent().add_child(ExplosionInstance)
+	ExplosionInstance.position = global_position
+	queue_free()
+	
+
+func _on_area_2d_body_entered(body):
+	if(body.has_method("takeDamage")):
+		body.takeDamage(Damage)
