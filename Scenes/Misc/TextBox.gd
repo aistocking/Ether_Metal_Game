f7eeb0a972fc165dd446c2d79a5e4e86abe45d74
Alex Stocking <alex.i.stocking@gmail.com>
2024-05-09 23:11:29 -0400

    feat: Working textbox that can be traveled through and ended

--- Scenes/Misc/TextBox.gd
+++ Scenes/Misc/TextBox.gd
@@ -2,21 +2,57 @@ extends CanvasLayer
 
 @onready var currentText = $MarginContainer/HSplitContainer/Box/MarginContainer/Text
 
-@onready var tween = get_tree().create_tween()
+var tween
 
-# Called when the node enters the scene tree for the first time.
+@onready var Player = get_tree().get_first_node_in_group("Player")
+
+var readRate : float = 0
+var counter : int = 0
+var curText : String
+var text0 : String = "Why must reploids fight one another?"
+var text1 : String = "I've had enough fighting!"
+var text2 : String = "......god X7 was bad."
+
+
 func _ready():
+	Player.changePlayerControl(false)
+	playText()
+
+func _process(delta):
+	currentText.visible_ratio = readRate
+
+func _input(event):
+	if Input.is_action_just_pressed("Face Buttons"):
+		if readRate < 1:
+			skipText()
+			return
+		playText()
+
+func playText():
 	resetText()
+	match counter:
+		0:
+			currentText.text = text0
+		1:
+			currentText.text = text1
+		2:
+			currentText.text = text2
+		3:
+			endDialogue()
+	counter += 1
+	tween = get_tree().create_tween()
+	tween.tween_property(self, "readRate", 1, 1)
 
 
-func newText(text):
-	resetText()
-	currentText = text
-	tween.tween_property(currentText, "Visible Ratio", 0, 1)
 
 func resetText():
 	currentText.text = ""
-	currentText.visible_ratio = 0
+	readRate = 0
 
 func skipText():
-	currentText.visible_ratio = 1
+	tween.kill()
+	readRate = 1
+
+func endDialogue():
+	Player.changePlayerControl(true)
+	queue_free()
