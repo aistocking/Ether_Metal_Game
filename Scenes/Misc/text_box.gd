2fde4011cf2137e11babef200a2dba3e0623fbd5
David Stocking <dmstocking@gmail.com>
2024-05-22 00:05:11 -0400

    feat: Rework Dialog System to be more Ren'Py like
    Uses await functions to simulate a single file representing an entire
    conversation. Started to add functionality for switching portraits and
    sides, but didn't get far enough.

--- Scenes/Misc/text_box.gd
+++ Scenes/Misc/text_box.gd
@@ -1,59 +1,51 @@
 class_name TextBox
 extends CanvasLayer
 
-@onready var currentText = $MarginContainer/HSplitContainer/Box/MarginContainer/Text
 
-var tween
+signal advanced
 
-@onready var Player = get_tree().get_first_node_in_group("Player")
+@onready var _current_text: Label = %DialogLabel
+@onready var _player: PlayerCharacter = get_tree().get_first_node_in_group("Player")
 
-var readRate : float = 0
-var counter : int = 0
-var curText : String
-var text0 : String = "Why must reploids fight one another?"
-var text1 : String = "I've had enough fighting!"
-var text2 : String = "......god X7 was bad."
+var _tween: Tween
+var _read_rate: float = 0
 
 
-func _ready():
-	Player.change_player_control(false)
-	playText()
+func _ready() -> void:
+	_player.change_player_control(false)
+	_say("Why must reploids fight one another?")
+	await advanced
+	_say("I've had enough fighting!")
+	await advanced
+	_say("......god X7 was bad.")
+	await advanced
+	_end_dialog()
 
-func _process(delta):
-	currentText.visible_ratio = readRate
 
-func _input(event):
+func _process(_delta: float) -> void:
+	pass
+
+
+func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("Face Buttons"):
-		if readRate < 1:
-			skipText()
+		if _current_text.visible_ratio < 1:
+			_display_full_text()
 			return
-		playText()
-
-func playText():
-	resetText()
-	match counter:
-		0:
-			currentText.text = text0
-		1:
-			currentText.text = text1
-		2:
-			currentText.text = text2
-		3:
-			endDialogue()
-	counter += 1
-	tween = get_tree().create_tween()
-	tween.tween_property(self, "readRate", 1, 1)
+		advanced.emit()
 
 
+func _say(text: String) -> void:
+	_current_text.visible_ratio = 0
+	_current_text.text = text
+	_tween = self.create_tween()
+	_tween.tween_property(_current_text, "visible_ratio", 1, 1)
 
-func resetText():
-	currentText.text = ""
-	readRate = 0
 
-func skipText():
-	tween.kill()
-	readRate = 1
+func _display_full_text() -> void:
+	_tween.kill()
+	_current_text.visible_ratio = 1
 
-func endDialogue():
-	Player.change_player_control(true)
+
+func _end_dialog() -> void:
+	_player.change_player_control(true)
 	queue_free()
