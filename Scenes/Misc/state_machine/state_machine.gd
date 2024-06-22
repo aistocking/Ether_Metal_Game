25cede06e38a4cf08160f2c68ec9bb3ddc493956
David Stocking <dmstocking@gmail.com>
2024-06-22 00:59:38 -0400

    refactor: Clean up StateMachine
    Just renamed properties and set types up better.

--- Scenes/Misc/state_machine/state_machine.gd
+++ Scenes/Misc/state_machine/state_machine.gd
@@ -1,42 +1,42 @@
 class_name StateMachine
 extends Node
 
-signal transitioned(state_name)
+signal transitioned(state_name: String)
 
-@export var initial_state := NodePath()
+@export var initial_state: State
 
-@onready var state: State = get_node(initial_state)
+@onready var _state := initial_state
 
 func _ready() -> void:
 	await owner.ready
 	
 	for child in get_children():
 		child.state_machine = self
-	state.enter()
-	emit_signal("transitioned", state.name)
+	_state.enter()
+	emit_signal("transitioned", _state.name)
 
 func _unhandled_input(event: InputEvent) -> void:
-	state.handle_input(event)
+	_state.handle_input(event)
 
 func _process(delta: float) -> void:
-	state.update(delta)
+	_state.update(delta)
 
 func _physics_process(delta: float) -> void:
-	state.physics_update(delta)
+	_state.physics_update(delta)
 
 func transition_to(target_state_name: String, msg: Dictionary = {}) -> void:
 	
 	if not has_node(target_state_name):
 		return
 
-	state.exit()
-	state = get_node(target_state_name)
-	state.enter(msg)
-	emit_signal("transitioned", state.name)
+	_state.exit()
+	_state = get_node(target_state_name)
+	_state.enter(msg)
+	emit_signal("transitioned", _state.name)
 
 func _takeDamage() -> void:
-	state.takeDamage()
+	_state.takeDamage()
 
 func _die() -> void:
-	state.die()
+	_state.die()
 
