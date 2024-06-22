7b8f25be901e611fd6d23723b6ecb9337ecbbdc1
David Stocking <dmstocking@gmail.com>
2024-06-22 01:01:16 -0400

    refactor: Reworked Damaged state
    Used await to simplify a little. Also noticed that if you get damaged in
    air the enemy that damaged you can count as a "floor". Really weird.
    Need to fix in the future.

--- Scenes/Player/States/damaged.gd
+++ Scenes/Player/States/damaged.gd
@@ -5,7 +5,6 @@ var player: CharacterBody2D
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@onready var timer = $Timer
 
 func enter(_msg := {}) -> void:
 	player = owner
@@ -13,22 +12,17 @@ func enter(_msg := {}) -> void:
 	player.velocity.y = -150
 	player.player_animations.play("Damaged_Start")
 	player.player_animations.queue("Damaged_Loop")
-	timer.start(0.35)
 	player.invincibility_timer.start(1.0)
 	player.effect_audio_player.set_stream(player.DAMAGED_AUDIO)
 	player.effect_audio_player.play()
 	player.change_player_control(false)
+	await get_tree().create_timer(0.35).timeout
+	player.change_player_control(true)
+	state_machine.transition_to("Idle")
+
 
 func physics_update(delta: float) -> void:
 	player.velocity.x += (player.facing_direction * -70) * delta
 	player.velocity.y += gravity * delta
 	
 	player.move_and_slide()
-
-func _on_timer_timeout():
-	timer.stop()
-	player.change_player_control(true)
-	if Input.is_action_pressed("Left") || Input.is_action_pressed("Right"):
-		state_machine.transition_to("Run")
-	else:
-		state_machine.transition_to("Idle")
