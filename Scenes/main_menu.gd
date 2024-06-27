1b3db739deceed1b9126222948c863cee09d3126
Alex Stocking <alex.i.stocking@gmail.com>
2024-06-27 13:36:10 -0400

    feat: Added animations to main menu for more player feedback

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -1,26 +1,40 @@
 extends Control
 
+signal shot_finished
+signal fade_finished
+
+const _CHARGE_SHOT_SCENE: PackedScene = preload("res://Scenes/Effects/plasma_shot.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$CenterContainer/Start.grab_focus()
-	Global.change_music("res://Sound/Music/03_Title.mp3")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
+func lily_shot_animation() -> void:
+	$EffectAudioPlayer.stream = load("res://Sound/BusterChargeShot.wav")
+	$AnimationPlayer.play("LilyShot")
+	await shot_finished
+	$AnimationPlayer.play("FadeOut")
 
 func _on_start_pressed() -> void:
+	lily_shot_animation()
+	await fade_finished
 	get_tree().change_scene_to_file("res://Scenes/stage_select.tscn")
 
 
 func _on_quit_pressed() -> void:
+	lily_shot_animation()
+	await fade_finished
 	get_tree().quit()
 
 
 func _on_options_pressed() -> void:
+	lily_shot_animation()
+	await fade_finished
 	get_tree().change_scene_to_file("res://Scenes/options_ui.tscn")
 
 
