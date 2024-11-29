2e13762ff677d2e9bb08c04fadbc17f994444aef
Alex Stocking <alex.i.stocking@gmail.com>
2024-11-28 19:43:40 -0500

    feat: Added basic platform object that can move

---
+++ Scenes/Stages/Objects/moving_platform.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@export var _speed: int = 0
+@export var _delay: float = 0
+
+@onready var _start_marker
+@onready var _end_marker
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
