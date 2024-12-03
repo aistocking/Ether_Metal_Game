0176b577332bd4501f74b1857a97341b75ed63dd
Alex Stocking <alex.i.stocking@gmail.com>
2024-12-03 14:52:52 -0500

    fix: Transition doors work nearly perfectly

--- Scenes/Player/cutscene.gd
+++ Scenes/Player/cutscene.gd
@@ -6,13 +6,15 @@ var player: CharacterBody2D
 func enter(_msg := {}) -> void:
 	player = owner
 	player.velocity = Vector2.ZERO
-	player.player_animations.stop()
+	player.player_animations.pause()
 	player.change_player_control(false)
+	player.toggle_collision()
 	player.process_mode = Node.PROCESS_MODE_ALWAYS
 
 func physics_update(delta: float) -> void:
 	player.move_and_slide()
 
 func exit() -> void:
+	player.toggle_collision()
 	player.change_player_control(true)
 	player.process_mode = Node.PROCESS_MODE_INHERIT
--- Scenes/Stages/Objects/transition_door.gd
+++ Scenes/Stages/Objects/transition_door.gd
@@ -2,13 +2,17 @@ extends Node2D
 
 var _player
 var _camera
-@export var bottomLeftBound: Marker2D
-@export var topRightBound: Marker2D
+@onready var _audio_player: EffectAudioPlayer = $EffectAudioPlayer
+@onready var _open_sound: AudioStream = load("res://Sound/Door Open.wav")
+@onready var _close_sound: AudioStream = load("res://Sound/Door Close.wav")
+@onready var _bottom_left_marker: Marker2D = $BottomLeftCameraBound
+@onready var _top_right_marker: Marker2D = $TopRightCameraBound
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_player = get_tree().get_first_node_in_group("Player")
 	_camera = get_tree().get_first_node_in_group("Camera")
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,26 +23,27 @@ func _process(delta):
 func _on_transition_trigger_body_entered(body):
 	if body == _player:
 		_player.enterCutsceneState()
+		Global.MusicPlayer.volume_db -= 5
 		get_tree().paused = true
+		_audio_player.play_sound(_open_sound)
 		$AnimationPlayer.play("Unseal")
 		await $AnimationPlayer.animation_finished
 		$AnimationPlayer.play("Open")
 		await $AnimationPlayer.animation_finished
-		_player.velocity.x = 45
-		_setCameraBounds(1)
-		await get_tree().create_timer(1).timeout
-		$AnimationPlayer.play("Open", -1, -1.5, true)
+		_player.velocity.x = 50
+		_player.player_animations.play()
+		_camera.change_camera_limits(_bottom_left_marker.global_position, _top_right_marker.global_position, 1.3, true)
+		await get_tree().create_timer(1.3).timeout
+		_player.player_animations.play("Idle")
+		_player.velocity.x = 0
+		_player.velocity.y = ProjectSettings.get_setting("physics/2d/default_gravity")
+		_audio_player.play_sound(_close_sound)
+		$AnimationPlayer.play("Open", -1, -2, true)
 		await $AnimationPlayer.animation_finished
 		$TransitionTrigger/CollisionShape2D.disabled = true
 		$BlockingCollision/CollisionShape2D.disabled = false
 		$AnimationPlayer.play("Lock")
 		await $AnimationPlayer.animation_finished
+		Global.MusicPlayer.volume_db = Global.get_music_volume()
 		_player.exitCutsceneState()
 		get_tree().paused = false
-
-func _setCameraBounds(transition_time: float) -> void:
-	_camera.change_camera_limits(bottomLeftBound.global_position, topRightBound.global_position, transition_time)
-
-func _on_stopping_trigger_body_entered(body):
-	if body == _player:
-		_player.velocity = Vector2.ZERO
