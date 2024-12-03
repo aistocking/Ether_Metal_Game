c586b152aeeda28900896e763f9c5368bb7550f7
Alex Stocking <alex.i.stocking@gmail.com>
2024-12-03 14:52:11 -0500

    fix: Reworked camera bound transitions

--- Scenes/Misc/camera_bounds.gd
+++ Scenes/Misc/camera_bounds.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @export var transition_time: float = 1.0
 @export var instant_start_trigger: bool = false
+@export var door_transition: bool = false
+
 var _camera
 var _player
 
@@ -10,7 +12,7 @@ func _ready():
 	_camera = get_tree().get_first_node_in_group("Camera")
 	_player = get_tree().get_first_node_in_group("Player")
 	if instant_start_trigger == true:
-		_camera.change_camera_limits($BottomLeftPosition.global_position, $TopRightPosition.global_position, 0)
+		_camera.change_camera_limits($BottomLeftPosition.global_position, $TopRightPosition.global_position, 0, false)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,9 +22,9 @@ func _process(delta):
 
 func _on_end_trigger_body_entered(body):
 	if body == _player:
-		_camera.change_camera_limits($BottomLeftPosition.global_position, $TopRightPosition.global_position, transition_time)
+		_camera.change_camera_limits($BottomLeftPosition.global_position, $TopRightPosition.global_position, transition_time, false)
 
 
 func _on_start_trigger_body_entered(body):
 	if body == _player:
-		_camera.change_camera_limits($BottomLeftPosition.global_position, $TopRightPosition.global_position, transition_time)
+		_camera.change_camera_limits($BottomLeftPosition.global_position, $TopRightPosition.global_position, transition_time, false)
--- Scenes/Player/main_camera.gd
+++ Scenes/Player/main_camera.gd
@@ -14,8 +14,15 @@ func _process(_delta):
 
 #With the proper 2D position markers from camera_bounds scene, hand over the global_position of each along with a transition time
 #Then transition from the old limits to the new ones with the transition time
-func change_camera_limits(bottomLeft: Vector2, topRight: Vector2, transitionTime: float):
-	var tweenTop = self.create_tween().tween_property(self, "limit_top", topRight.y, transitionTime).set_trans(Tween.TRANS_CUBIC)
-	var tweenBottom = self.create_tween().tween_property(self, "limit_bottom", bottomLeft.y, transitionTime).set_trans(Tween.TRANS_CUBIC)
-	var tweenLeft = self.create_tween().tween_property(self, "limit_left", bottomLeft.x, transitionTime).set_trans(Tween.TRANS_CUBIC)
-	var tweenRight = self.create_tween().tween_property(self, "limit_right", topRight.x, transitionTime).set_trans(Tween.TRANS_CUBIC)
+func change_camera_limits(bottomLeft: Vector2, topRight: Vector2, transitionTime: float, doorTransition: bool):
+	if doorTransition:
+		var tweenTop = self.create_tween().tween_property(self, "limit_top", topRight.y, transitionTime).set_trans(Tween.TRANS_EXPO)
+		var tweenRight = self.create_tween().tween_property(self, "limit_right", topRight.x, transitionTime).set_trans(Tween.TRANS_EXPO)
+		limit_bottom = bottomLeft.y
+		limit_left = bottomLeft.x
+	else:
+		var tweenTop = self.create_tween().tween_property(self, "limit_top", topRight.y, transitionTime).set_trans(Tween.TRANS_EXPO)
+		var tweenBottom = self.create_tween().tween_property(self, "limit_bottom", bottomLeft.y, transitionTime).set_trans(Tween.TRANS_EXPO)
+		var tweenLeft = self.create_tween().tween_property(self, "limit_left", bottomLeft.x, transitionTime).set_trans(Tween.TRANS_EXPO)
+		var tweenRight = self.create_tween().tween_property(self, "limit_right", topRight.x, transitionTime).set_trans(Tween.TRANS_EXPO)
+	
