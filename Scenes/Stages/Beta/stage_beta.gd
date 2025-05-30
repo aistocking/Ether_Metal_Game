6b808c403d795c7224b6182a08adb16300d5430c
David Stocking <dmstocking@gmail.com>
2025-05-29 20:22:01 -0400

    refactor: Cleanup Stage Beta

--- Scenes/Stages/Beta/stage_beta.gd
+++ Scenes/Stages/Beta/stage_beta.gd
@@ -1,15 +1,9 @@
 extends Node2D
 
 
-var DialogueBoxResource: Resource = preload("res://Scenes/Misc/text_box.tscn")
-
-var _bg_gate1 := false
-var _bg_gate2 := false
-
-@onready var bg_trigger2 := $StageTriggers/BGTrigger2
-
 @onready var _stage_bgm: AudioStream = preload("res://Sound/Music/Svarog_Stage_BGM.mp3")
-@onready var _enemy: PackedScene = preload("res://Scenes/Enemies/base_enemy.tscn")
+var _dialog_resource: Resource = preload("res://Scenes/Misc/text_box.tscn")
+
 
 func _ready() -> void:
 	Global.change_music(_stage_bgm)
@@ -17,23 +11,17 @@ func _ready() -> void:
 	RenderingServer.set_default_clear_color(Color(0,0,0))
 
 
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	var DialogueBoxInst = DialogueBoxResource.instantiate()
-	add_child(DialogueBoxInst)
-	$StageTriggers/CutsceneTrigger.queue_free()
+func _on_area_2d_body_entered(_body: Node2D) -> void:
+	var dialog: Node = _dialog_resource.instanciate()
+	add_child(dialog)
+	%CutsceneTrigger.queue_free()
 
 
 func _on_bg_trigger_1_body_entered(body):
-	$BGs/BGRamps.visible = true
-	$BGs/MGRamps.visible = true
-	$BGs/MGGirders.visible = false
+	%Zone1.visible = true
+	%Zone2.visible = false
 
 
 func _on_bg_trigger_2_body_entered(body):
-	$BGs/BGRamps.visible = true
-	$BGs/MGRamps.visible = false
-	$BGs/MGGirders.visible = true
+	%Zone1.visible = false
+	%Zone2.visible = true
