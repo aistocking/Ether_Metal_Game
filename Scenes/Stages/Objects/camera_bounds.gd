4713ff58c7c69f5c52ab71bcc398913fdddff7e7
Alex Stocking <alex.i.stocking@gmail.com>
2025-06-27 10:40:37 -0400

    fix: Changed camera bounds to be a single collision object that can form to the stage

--- Scenes/Stages/Objects/camera_bounds.gd
+++ Scenes/Stages/Objects/camera_bounds.gd
@@ -7,7 +7,6 @@ extends Node2D
 var _camera
 var _player
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	_camera = get_tree().get_first_node_in_group("Camera")
 	_player = get_tree().get_first_node_in_group("Player")
@@ -15,16 +14,7 @@ func _ready():
 		_camera.change_camera_limits($BottomLeftPosition.global_position, $TopRightPosition.global_position, 0, false)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
-
-func _on_end_trigger_body_entered(body):
-	if body == _player:
-		_camera.change_camera_limits($BottomLeftPosition.global_position, $TopRightPosition.global_position, transition_time, false)
-
-
-func _on_start_trigger_body_entered(body):
+func _on_area_trigger_body_entered(body):
 	if body == _player:
 		_camera.change_camera_limits($BottomLeftPosition.global_position, $TopRightPosition.global_position, transition_time, false)
